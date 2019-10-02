f25972582e11f039658cf2eb618c3f279b2eb3da
Jonas Allmann <j_allmann@campus.tu-berlin.de>
2019-10-02 18:51:47 +0200

    working POC for listener replacement

--- background/background.js
+++ background/background.js
@@ -9,5 +9,47 @@ async function registerContentScript() {
   });
 }
 
-console.log("Background script is running. Starting to wait.")
+console.log("Background script is running. Starting to wait.");
 setTimeout(registerContentScript, 5000);
+
+function addPersistentWebRequestListener() {
+  browser.webRequest.onBeforeRequest.addListener(
+    dummyWebRequestListener,
+    {urls: ["<all_urls>"]},
+    ["blocking"]
+  );
+}
+let detailsBuffer = [];
+console.log("Blocking webRequest listener was added.");
+addPersistentWebRequestListener();
+
+function dummyWebRequestListener(details) {
+  console.log("Request intercepted blocking: " + details.requestId);
+  detailsBuffer.push(details);
+  return new Promise((resolve, reject) => {
+    console.log("Register listener that resolves the blocking promise.");
+    window.addEventListener("promiseResolve", resolve);
+    browser.runtime.onMessage.addListener(() => {resolve();});
+  }).then(function() {
+    return {};
+  });
+}
+
+function removeListener() {
+  console.log("Add new listener and remove old one.");
+  browser.webRequest.onBeforeRequest.addListener(
+    realListener,
+    {urls: ["<all_urls>"]},
+    ["blocking"]
+  );
+  browser.webRequest.onBeforeRequest.removeListener(dummyWebRequestListener);
+  let releaseEvent = new CustomEvent("promiseResolve");
+  window.dispatchEvent(releaseEvent);
+  detailsBuffer.forEach(realListener);
+}
+
+function realListener(details) {
+  console.log("simply intercept request: " + details.requestId)
+}
+
+setTimeout(removeListener, 5000);
--- content/content.js
+++ content/content.js
@@ -1,7 +1,14 @@
 function main() {
   console.log("Content script is being executed.");
   let title = document.querySelector("#title1");
+  wait(5000);
+  console.log("Content script finished waiting");
   title.innerHTML = "Content script executed last.";
+}
+
+function wait(ms) {
+    var start = +(new Date());
+    while (new Date() - start < ms);
 }
 
 main();
